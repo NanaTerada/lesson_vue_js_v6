0d17a6c72c93822fb0b4ac9cf8329d6f4e53497f
n2 <7worki.nprogress@gmail.com>
2020-06-03 16:26:44 +0900

    #05 v-onでイベントを設定しよう

--- js/main.js
+++ js/main.js
@@ -4,11 +4,21 @@
 var vm = new Vue({
     el: '#app',
     data: {
+        newItem:'',
     todos:[
         'task1',
         'task2',
         'task3',
     ]
+    },
+    methods: {
+        // addItem: function(e) {
+        //     e.preventDefault();
+        //     this.todos.push(this.newItem);
+        // }
+        addItem: function() {
+            this.todos.push(this.newItem);
+        }
     }
     });
 })();
